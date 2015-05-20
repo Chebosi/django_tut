2e5551589b6ea7c2bba36fc5efc108ee99c4b56d
Ramson Chebosi <ramson.chebosi@savannahinformatics.com>
2015-05-20 16:21:56 +0300

    TUTORIAL TWO: MADE POLL APP MODIFIABLE

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
 # Register your models here.
+#make poll app modifiable
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
