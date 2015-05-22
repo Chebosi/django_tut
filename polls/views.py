450536511f370da675f4c9093c2757819c2dd160
Ramson Chebosi <ramson.chebosi@savannahinformatics.com>
2015-05-22 05:15:29 +0300

    CLEANING UP BEFORE STARTING GENERIC VIEWS TUT 4
    Polls views.py wasnt catching xception as expected. Import http404 was missing

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
