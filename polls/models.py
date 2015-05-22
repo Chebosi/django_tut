501ecdee60ddddba98862a8cdf749cdb77d644cb
Ramson Chebosi <ramson.chebosi@savannahinformatics.com>
2015-05-22 09:16:15 +0300

    ENSURING THAT WAS PUBLISHED RECENTLY WORKS CORRECTLY
    Writing a test  for was published recently remove bug that sets
    future date as was published recently

--- polls/models.py
+++ polls/models.py
@@ -10,14 +10,35 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
+    
     def __str__(self):              # __unicode__ on Python 2
         return self.question_text
 
